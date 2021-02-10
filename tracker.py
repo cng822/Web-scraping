15115194b98e325fc34a2cb517c8fe0d60296b9b
Carrie <cng822@aucklanduni.ac.nz>
2021-02-10 22:56:12 +1300

    Update tracker.py

--- tracker.py
+++ tracker.py
@@ -10,30 +10,50 @@ from selenium.webdriver.support import expected_conditions as EC
 
 import time
 
-PATH = 'C:\Program Files (x86)\chromedriver.exe'
-URL = 'https://www.etsy.com/nz/listing/884020481/anklet-bracelet-gold-chain-anklet-thick?ga_order=most_relevant&ga_search_type=all&ga_view_type=gallery&ga_search_query=&ref=sc_gallery-1-2&plkey=811b0d827e70a34e51d0455890ae256faf698715%3A884020481&pro=1'
+# PATH = 'C:\Program Files (x86)\chromedriver.exe'
 
-driver = webdriver.Chrome(PATH)
+# driver = webdriver.Chrome(PATH)
 headers = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36'}
 
 def check_price():
+    URL = input("Please the URL link of the item you want to check: \n")
     page = requests.get(URL, headers = headers)
 
     soup = BeautifulSoup(page.content, 'html.parser')
 
     title = soup.find("h2", class_="wt-screen-reader-only").get_text()
+    print(f'Item you are after is: {title}')
+
     current_price = soup.find("p", class_="wt-text-title-03 wt-mr-xs-2").get_text()
     saved = soup.find("p", class_="wt-text-caption wt-text-slime")
     price = re.findall('\d+\.\d+', current_price)
+
     if (saved == None):
         saved = 'Not on sale'
     else:
         saved = saved.get_text()
         saved = saved.strip()
 
-    if (float(price[0]) < 35):
+
+    while (True):
+        desired_price = input("The price you are willing to spend is : (Rounded to the nearest whole) \n")
+        try:
+            val = int(desired_price)
+            print("Input number is: ", val)
+            break
+        except ValueError:
+            try:
+                float(desired_price)
+                print("Input is an float number.")
+                print("Input number is: ", val)
+                break
+            except ValueError:
+                print("This is not a number. Please enter a valid number")
+    print(val)
+    if (float(price[0]) < val):
+        print('Reached')
         send_mail(saved, current_price)
-        check_stock()
+        # check_stock()
 
 def send_mail(saved, current_price):
     server = smtplib.SMTP('smtp.gmail.com', 587)
@@ -58,22 +78,25 @@ def send_mail(saved, current_price):
 
     server.quit()
 
-def check_stock(): 
-    driver.get("https://www.etsy.com/nz")
-    print(driver.title)
+# def check_stock(): 
+#     driver.get("https://www.etsy.com/nz")
+#     print(driver.title)
 
-    search = driver.find_element_by_name("s")
-    search.send_keys("test")
-    search.send_keys(Keys.RETURN)
+#     search = driver.find_element_by_name("s")
+#     search.send_keys("test")
+#     search.send_keys(Keys.RETURN)
 
-    try: 
-        main = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.ID, "main"))
-        )
-    except:
-        driver.quit()
+#     link = driver.find_element_by_link_text("Spotify Frame")
+#     link.click()
 
-    main = driver.find_element_by_id()
-    driver.quit()
+#     try: 
+#         main = WebDriverWait(driver, 10).until(
+#             EC.presence_of_element_located((By.ID, "main"))
+#         )
+#     except:
+#         driver.quit()
+
+#     main = driver.find_element_by_id()
+#     driver.quit()
 
 check_price()
